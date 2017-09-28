849f9df11bddc103242c67b22d6aa86d2fae5ca9
Gregor Schulte <gregor.schulte@uni-tuebingen.de>
2017-09-28 13:48:59 +0200

    Initial Commit, WOrking on COM3

--- OptoControl/Form1.Designer.cs
+++ OptoControl/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OptoControl
 {
-    partial class Form1
+    partial class OptoControl
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.MyButtonEnable = new System.Windows.Forms.Button();
+            this.MyButtonDirection = new System.Windows.Forms.Button();
+            this.MyTrackBarInterval = new System.Windows.Forms.TrackBar();
+            this.MyLabelInterval = new System.Windows.Forms.Label();
+            this.MySerialPort = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.MyTrackBarInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MyButtonEnable
+            // 
+            this.MyButtonEnable.Location = new System.Drawing.Point(12, 12);
+            this.MyButtonEnable.Name = "MyButtonEnable";
+            this.MyButtonEnable.Size = new System.Drawing.Size(129, 52);
+            this.MyButtonEnable.TabIndex = 1;
+            this.MyButtonEnable.Text = "Enable";
+            this.MyButtonEnable.UseVisualStyleBackColor = true;
+            this.MyButtonEnable.Click += new System.EventHandler(this.MyButtonEnable_Click);
+            // 
+            // MyButtonDirection
+            // 
+            this.MyButtonDirection.Location = new System.Drawing.Point(514, 12);
+            this.MyButtonDirection.Name = "MyButtonDirection";
+            this.MyButtonDirection.Size = new System.Drawing.Size(153, 52);
+            this.MyButtonDirection.TabIndex = 2;
+            this.MyButtonDirection.Text = "Turn Right";
+            this.MyButtonDirection.UseVisualStyleBackColor = true;
+            this.MyButtonDirection.Click += new System.EventHandler(this.MyButtonDirection_Click);
+            // 
+            // MyTrackBarInterval
+            // 
+            this.MyTrackBarInterval.LargeChange = 1;
+            this.MyTrackBarInterval.Location = new System.Drawing.Point(12, 70);
+            this.MyTrackBarInterval.Maximum = 30;
+            this.MyTrackBarInterval.Minimum = 2;
+            this.MyTrackBarInterval.Name = "MyTrackBarInterval";
+            this.MyTrackBarInterval.Size = new System.Drawing.Size(655, 69);
+            this.MyTrackBarInterval.TabIndex = 3;
+            this.MyTrackBarInterval.Value = 5;
+            this.MyTrackBarInterval.Scroll += new System.EventHandler(this.MyTrackBarInterval_Scroll);
+            // 
+            // MyLabelInterval
+            // 
+            this.MyLabelInterval.AutoSize = true;
+            this.MyLabelInterval.Location = new System.Drawing.Point(290, 28);
+            this.MyLabelInterval.Name = "MyLabelInterval";
+            this.MyLabelInterval.Size = new System.Drawing.Size(51, 20);
+            this.MyLabelInterval.TabIndex = 4;
+            this.MyLabelInterval.Text = "label1";
+            this.MyLabelInterval.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MySerialPort
+            // 
+            this.MySerialPort.PortName = "COM3";
+            // 
+            // OptoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(679, 281);
+            this.Controls.Add(this.MyLabelInterval);
+            this.Controls.Add(this.MyTrackBarInterval);
+            this.Controls.Add(this.MyButtonDirection);
+            this.Controls.Add(this.MyButtonEnable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "OptoControl";
+            this.Text = "OptoControl";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Optocontrol_Close);
+            this.Load += new System.EventHandler(this.OptoControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MyTrackBarInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button MyButtonEnable;
+        private System.Windows.Forms.Button MyButtonDirection;
+        private System.Windows.Forms.TrackBar MyTrackBarInterval;
+        private System.Windows.Forms.Label MyLabelInterval;
+        private System.IO.Ports.SerialPort MySerialPort;
     }
 }
 
--- OptoControl/Form1.cs
+++ OptoControl/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
 
 namespace OptoControl
 {
-    public partial class Form1 : Form
+    public partial class OptoControl : Form
     {
-        public Form1()
+        //private SerialPort MySerialPort = new SerialPort("COM3",9600);
+
+        private bool enabled = false;
+        private bool direction = false;
+
+
+        public OptoControl()
         {
             InitializeComponent();
+        }
+
+        private void MyButtonSend_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OptoControl_Load(object sender, EventArgs e)
+        {
+            MySerialPort.Open();
+            MySerialPort.WriteLine("125 0 0");
+            MySerialPort.Close();
+            MyLabelInterval.Text = MyTrackBarInterval.Value.ToString();
+
+        }
+        
+        private void Optocontrol_Close(object sender, EventArgs e)
+        {
+            enabled = false;
+            MySerialWriteLine();
+
+        }
+
+        private void MyButtonEnable_Click(object sender, EventArgs e)
+        {
+
+            enabled = !enabled;
+            if (enabled)
+            {
+                MyButtonEnable.Text = "Disable";
+            }
+            else
+            {
+                MyButtonEnable.Text = "Enable";
+            }
+            MySerialWriteLine();
+
+
+        }
+
+        public void MySerialWriteLine()
+        {
+            MySerialPort.Open();
+            MySerialPort.WriteLine(MyTrackBarInterval.Value.ToString() + " " + Convert.ToInt16(direction).ToString()+ " " + Convert.ToInt16(enabled).ToString());
+            MySerialPort.Close();
+        }
+
+        private void MyButtonDirection_Click(object sender, EventArgs e)
+        {
+            direction = !direction;
+            if (direction)
+            {
+                MyButtonDirection.Text = "Turn Left";
+            }
+            else
+            {
+                MyButtonDirection.Text = "Turn Right";
+            }
+            MySerialWriteLine();
+
+        }
+
+        private void MyTrackBarInterval_Scroll(object sender, EventArgs e)
+        {
+            //MyButtonSend.Text = MyTrackBarInterval.Value.ToString();
+            MySerialWriteLine();
+            MyLabelInterval.Text = MyTrackBarInterval.Value.ToString();
         }
     }
 }
